13d0d145b446621d8d2f51770e40505660be4942
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-20 02:54:00 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,7 +11,7 @@ class SearchesController < ApplicationController
        req.params['v'] = '20160201'
        req.params['near'] = params[:zipcode]
        req.params['query'] = 'coffee shop'
-    
+
      end
      body = JSON.parse(@resp.body)
      if @resp.success?
